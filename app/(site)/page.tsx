ba125ac1462cc664dd7e303d2bca52df920daaee
khanguyenh10 <khanguyenh10dev@gmail.com>
2026-03-28 00:13:31 +0700

    Update init

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,74 +1,11 @@
-// "use client"
-import Image from 'next/image'
-import Goto from '../(component)/Goto';
-type Props = {
-  children?: React.ReactNode
-}
+import React from 'react'
 
-//Lấy danh sách sản phẩm action server
-const getAllProductApi = async () => {
-  try {
-    const res = await fetch("https://apistore.cybersoft.edu.vn/api/Product");
-    const data = await res.json();
-    return data.content;
-  } catch (error) {
-    console.error("Error fetching products:", error);
-    return [];
-  }
-}
+type Props = {}
 
-
-
-
-
-const page = async (props: Props) => {
-  // const router = useRouter();
-  const arrProduct: any = await getAllProductApi();
-  console.log("arrproduct", arrProduct)
-  // const [arrProduct, setArrProduct] = useState([]);
-
-  // const getAllProduct = async () => {
-  //   try {
-  //     const res = await fetch("https://apistore.cybersoft.edu.vn/api/Product");
-  //     const data = await res.json();
-  //     setArrProduct(data.content);
-  //   } catch (error) {
-  //     console.error("Error fetching products:", error);
-  //   }
-  // };
-
-  // React.useEffect(() => {
-  //   getAllProduct();
-  // }, []);
-
+const page = (props: Props) => {
   return (
     <div>
-      <div className='container'>
-
-        <h1>Home page</h1>
-        {/* <Image src={'https://apistore.cybersoft.edu.vn/images/van-old-school.png'} width={500} height={500} quality={75} alt='...' style={{width:"300px" , height:"auto"}} /> */}
-        <div className='row'>
-          {arrProduct.map((item: any, index: number) => {
-            return <div className='col-3 my-2' key={index}>
-              <div className="card">
-                <Image src={item.image} width={500} height={500} quality={75} alt='...' style={{ width: "100%", height: "200px", objectFit: "cover" }} />
-                <div className="card-body">
-                  <h5 className="card-title">{item.name}</h5>
-                  <p className="card-text">{item.price}</p>
-                  <button className='btn btn-primary'>View detail</button>
-                </div>
-              </div>
-            </div>
-          })
-          }
-        </div>
-
-      </div>
-      <Goto />
-      {/* 
-      <button className='btn btn-success' onClick={()=>{
-        router.push("/about")
-      }}>Go to about</button> */}
+      <button className=' bg-amber-950 text-white '>action</button>
     </div>
   )
 }
