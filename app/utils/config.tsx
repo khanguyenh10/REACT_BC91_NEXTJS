c8590d6d0e3c7b355d718b65f3a6d00682a926a7
khanguyenh10dev <khanguyenh10dev@gmail.com>
2026-09-10 17:03:16 +0700

    Update

--- app/utils/config.tsx
+++ app/utils/config.tsx
@@ -1,6 +1,7 @@
 
 //hẳng số
-export const DOMAIN = 'https://airbnbnew.cybersoft.edu.vn/api';
+// export const DOMAIN = 'https://airbnbnew.cybersoft.edu.vn/api';
+export const DOMAIN = 'https://node-bc91-ts.onrender.com/api/v1';
 export const ACCESSTOKEN = 'accessToken';
 export const USER = 'user';
 export const TOKENCYBERSOFT = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCA5MSIsIkhldEhhblN0cmluZyI6IjAyLzA5LzIwMjYiLCJIZXRIYW5UaW1lIjoiMTc4ODMwNzIwMDAwMCIsIm5iZiI6MTc1OTk0MjgwMCwiZXhwIjoxNzg4NDU0ODAwfQ.3f2gLYDZla_lDH4GWmfgSe9Il_QHrpoHIWhW6FSKTi8';
